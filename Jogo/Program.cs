58c6a2bdf0bc649e709f0144b4df1f5bce519dc4
Juliana Lobo <sousalobojuliana@gmail.com>
2017-11-29 10:50:21 -0200

    Começando a implementar o tabuleiro

--- Jogo/Program.cs
+++ Jogo/Program.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tabuleiro;
+using tabuleiro;
 
 namespace Jogo {
     class Program {
         static void Main(string[] args) {
 
-            Posicao P;
-            P = new Posicao( 3, 4);
-
-            Console.WriteLine("Posicao: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadLine();
         }
     }
