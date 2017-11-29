6c8880be097a2fb4d6a553c54a60ef9ef1406462
Juliana Lobo <sousalobojuliana@gmail.com>
2017-11-29 11:54:38 -0200

    Comecando a colocar pecas no tabuleiro :)

--- Jogo/Tela.cs
+++ Jogo/Tela.cs
@@ -12,12 +12,11 @@ namespace Jogo {
             for(int i=0; i<tab.linhas; i++) {
                 for(int j=0; j<tab.colunas; j++) {
                     if (tab.peca(i,j) == null) {
-                        Console.Write("-  ");
+                        Console.Write("- ");
                     }
                     else {
                         Console.Write(tab.peca(i,j) + " ");
                     }
-                    Console.Write(tab.peca(i, j) + " ");
                 }
                 Console.WriteLine();
             }
