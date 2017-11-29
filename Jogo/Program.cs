cc71490356bd44fb0799b53e34e6432eaea4a6a7
Juliana Lobo <sousalobojuliana@gmail.com>
2017-11-29 10:27:07 -0200

    Criando a classe posicao

--- Jogo/Program.cs
+++ Jogo/Program.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace Jogo {
     class Program {
         static void Main(string[] args) {
+
+            Posicao P;
+            P = new Posicao( 3, 4);
+
+            Console.WriteLine("Posicao: " + P);
+            Console.ReadLine();
         }
     }
 }
