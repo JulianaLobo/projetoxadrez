5b82cd240e62a956a49dc361ba6111ae236194ab
Juliana Lobo <sousalobojuliana@gmail.com>
2017-12-13 11:45:03 -0200

    Controlando as pecas capturadas

--- Jogo/Program.cs
+++ Jogo/Program.cs
@@ -18,10 +18,7 @@ namespace Jogo {
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
@@ -33,6 +30,7 @@ namespace Jogo {
                         Console.Clear();
                         Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
+                        Console.WriteLine();
                         Console.Write("Destino: ");
                         Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
                         partida.validarPosicaoDeDestino(origem, destino);
